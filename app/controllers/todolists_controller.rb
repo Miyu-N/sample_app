9e92ce7bf7c8a8327e81d912d73a659013befe67
Miyu-N <myu.star1217@gmail.com>
2021-04-25 05:29:55 +0000

    [Finish]Rails7 画像投稿機能の追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -38,5 +38,5 @@ end
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f3fcdf2765606cde4ae47d8784aa9c59dbad4b7ab63a68c0fa1e0112d54ce567cfbe24ff48d6088968c92624962aa6faf1260f34f31008614f6d9e86bad07ff0'
+
